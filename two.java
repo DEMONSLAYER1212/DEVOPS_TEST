293729e67984d3ce9006e651c9fd4173201a484e
= <94789102+SURESH2218@users.noreply.github.com>
2024-05-21 16:30:43 +0530

    java file updated

--- two.java
+++ two.java
@@ -1,12 +1,16 @@
-class Main {
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+        
+        // Call the addNumbers method and print the result
+        int result = addNumbers(5, 3);
+        System.out.println("The sum of 5 and 3 is: " + result);
+    }
 
-  public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 20;
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
-  }
+    // Method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
 }
+
